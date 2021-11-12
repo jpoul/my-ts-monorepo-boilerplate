f226ad21021fa6bf5ab7dd5af2779bc6e9ed7835
John Pouliezos <jpouliezos@gmail.com>
2021-11-12 14:56:33 +0200

    resolved npm and project reference loading

--- packages/sampleConsumer/src/index.ts
+++ packages/sampleConsumer/src/index.ts
@@ -1,7 +1,9 @@
+import { IFoo } from '@my-ts-monorepo-boilerplate/sample';
 export interface IBoo {
   age: number;
 }
 
-export class Boo implements IBoo {
+export class Boo implements IBoo, IFoo {
+  public name: string = 'My name';
   public age: number = 18;
 }
